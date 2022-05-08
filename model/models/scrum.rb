c6975ea35eb5bf52ceaeb7dc86c155e9fb81a18a
Sherif Nafee <shico.m.nafee@gmail.com>
2022-05-08 15:17:03 +0300

    Create base methods for scrum states

---
+++ model/models/scrum.rb
@@ -0,0 +1,92 @@
+require_relative '../process.rb'
+require_relative '../anomaly/anomaly'
+
+class ScrumState < DevelopmentProcessState
+    attr_accessor :anomalies,
+                  :actions
+  
+    def initialize(backlog, anomalies_context)
+      @backlog = backlog
+      @anomalies_context = anomalies_context
+    end
+  
+    def advance_task(feature)
+      raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+    def update
+      @anomalies = @anomalies.select {|anomaly| anomaly.difficulty > 0}
+      @anomalies = @anomalies + @anomalies_context.generate_anomalies(self)
+    end
+end
+
+class ScrumSprintPlanning < ScrumState
+
+    def initialize(backlog, anomalies_context, sprint_length)
+        super
+        @sprint_length = sprint_length
+        @tasks_added = false
+    end
+    
+    def can_transition_state?(state)
+        @tasks_added
+    end
+
+    def update()
+        choose_tasks_for_sprint() if !@tasks_added
+    end
+
+    def choose_tasks_for_sprint()
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+
+end
+
+class ScrumDaily < ScrumState
+
+    def initialize(backlog, anomalies_context, sprint_length, sprint_tasks)
+        super
+        @sprint_length = sprint_length
+        @current_sprint_days = 0
+        @sprint_tasks = sprint_tasks
+    end
+
+    def can_transition_state?(state)
+        (@current_sprint_days >= @sprint_length)
+    end
+
+    def update()
+        progress_tasks()
+        finish_sprint_day()
+    end
+
+    def finish_sprint_day()
+        @current_sprint_days+=1
+    end
+
+    def progress_tasks()
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+end
+
+class ScrumReview < ScrumState
+
+    def initialize(backlog, anomalies_context, sprint_length, sprint_tasks)
+        super
+        @sprint_length = sprint_length
+        @tasks_sent_back = false
+        @sprint_tasks = sprint_tasks
+    end
+
+    def can_transition_state?(state)
+        @tasks_sent_back
+    end
+
+    def update()
+        send_sprint_tasks_to_backlog() if !@tasks_sent_back
+    end
+
+    def send_sprint_tasks_to_backlog()
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+end 
+
