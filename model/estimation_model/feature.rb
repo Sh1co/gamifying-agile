df9085b9b69bf5b118ff051e97bfc3bf9f1cfa25
Sherif Nafee <shico.m.nafee@gmail.com>
2022-06-29 20:09:37 +0300

    Make features lists of tasks

--- model/estimation_model/feature.rb
+++ model/estimation_model/feature.rb
@@ -1,16 +1,12 @@
 class Feature
-  attr_reader :analysis_difficulty,
-              :implementation_difficulty,
-              :testing_difficulty,
+  attr_reader :tasks,
               :difficulty,
               :anomalies
   attr_accessor :completed
 
-  def initialize(analysis_difficulty, implementation_difficulty, testing_difficulty)
-    @difficulty = analysis_difficulty + implementation_difficulty + testing_difficulty
-    @analysis_difficulty = analysis_difficulty
-    @implementation_difficulty = implementation_difficulty
-    @testing_difficulty = testing_difficulty
+  def initialize(tasks)
+    @tasks = tasks
+    @difficulty = tasks.reduce(0) {|acc, task| acc + task.difficulty}
     @completed = 0
     @anomalies = []
   end
--- model/estimation_model/spec.rb
+++ model/estimation_model/spec.rb
@@ -1,12 +1,14 @@
 class Spec
   attr_reader :time_required,
               :feature,
-              :required_skills
+              :required_skills,
+              :difficulty
   attr_accessor :is_worked_on
 
   def initialize(feature, required_skills)
     @feature = feature
     @required_skills = required_skills
+    @difficulty = required_skills.reduce(0) {|acc, skill| acc + skill.level}
     @is_worked_on = false
   end
 end
