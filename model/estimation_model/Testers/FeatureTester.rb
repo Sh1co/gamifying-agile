02948d26352c1f9700447dbe53861510a8dcb123
Sherif Nafee <shico.m.nafee@gmail.com>
2022-06-30 19:36:06 +0300

    Create tester for feature

---
+++ model/estimation_model/Testers/FeatureTester.rb
@@ -0,0 +1,16 @@
+require_relative '../spec'
+require_relative '../skill'
+require_relative '../feature'
+
+analysis_task = Spec.new([Skill.new("analysis", 8)], "analysis", 7)
+development_task = Spec.new([Skill.new("development", 6)], "development", 4)
+testing_task = Spec.new([Skill.new("testing", 7)], "testing", 5)
+
+test_feature = Feature.new([analysis_task, development_task, testing_task])
+
+nxt_task = test_feature.get_next_task
+
+while !nxt_task.nil? do
+    print nxt_task.task_type, "\n"
+    nxt_task = test_feature.get_next_task
+end
--- model/estimation_model/feature.rb
+++ model/estimation_model/feature.rb
@@ -14,9 +14,9 @@ class Feature
   end
 
   def get_next_task()
-      if task_ind < tasks.length
-        task_ind += 1
-        return tasks[task_ind-1]
+      if @task_ind < @tasks.length
+        @task_ind += 1
+        return @tasks[@task_ind-1]
       end
       return nil
   end
